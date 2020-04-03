ea25f1933642dee69e2a587eb5e7b1c7577c876e
Nikitos <pocketkurt@gmail.com>
2020-04-04 01:41:34 +0300

    1
    1

---
+++ List.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <memory>
+
+using namespace std;
+
+//Данные в буффер
+struct Node {
+	int data;
+	shared_ptr<Node> next;
+};
+
+//Реализация буфера. 
+class Queue {
+private:
+	
+	shared_ptr<Node> top;
+	shared_ptr<Node> bottom;
+	unsigned int count = 0;
+
+public:
+
+	Queue(unsigned int size) {
+	
+		shared_ptr<Node> current(new Node);
+		shared_ptr<Node> begin(new Node);
+		current->data = 0;
+		current->next = current;
+		begin = current;
+
+		this->top = begin;
+		for (int i = 1; i < size; i++) {
+			shared_ptr<Node> tmp(new Node);
+			bottom = tmp;
+			tmp->data = 0;
+			tmp->next = begin;
+			current->next = tmp;
+			current = current->next;
+		}
+	}
+
+	//Добавение элемента в буфер
+	void enqueue(int value){
+
+		bottom = bottom->next;
+		bottom->data = value;
+		count++;
+
+	}
+
+	//Удаление элемента из буфера	
+	int dequeue(){
+		if (count != 0) {
+			int deleted;
+			deleted = top->data;
+			top->data = 0;
+			top = top->next;
+			count--;
+			return deleted;
+		}
+		else {
+			return -1;
+		}
+	}
+
+	//Возвращает количество элементов в буфере
+	unsigned int size() {
+		return count;
+	}
+
+	//Флаг пустого буфера
+	bool isEmpty(){
+		if (count == 0) { return true; }
+		else return false;
+	}
+
+	//Возвращение первого элемента в очереди
+	int first() {
+		return top->data;
+	}
+
+};
---
+++ Vector.cpp
@@ -0,0 +1,113 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace  std;
+
+class Queue {
+
+private:
+	
+	unsigned int top_;   //позиция верхнего элемента
+	unsigned int bottom_;//позиция последнего элемента
+	unsigned int size_;  //размер буфера
+	unsigned int real_size_; //количество элементов в буфере
+
+public:
+	
+	vector<int> buff;
+
+	//конструктор
+	Queue(unsigned int size){
+		top_ = 0;
+		bottom_ = 0;
+		size_ = 0;
+		buff.reserve(size);
+		for (int i = 0; i < value; i++) {
+			buff.push_back(0);
+			this->size_++;
+		}
+	}
+
+	//добавление элемента в буфер
+	void enqueue(int value) {
+		if (bottom_ < size_) {
+			this->buff[bottom_] = value;
+			bottom_ += 1;
+		}
+		else {
+			this->buff[0] = value;
+			this->bottom_ = 1;
+		}
+		if (real_size_ < size_) {
+			this->real_size_++;
+		}
+		else {
+			this->real_size_ = 10;
+		}
+	}
+
+	//удаление элемента из буфера
+	int dequeue() {
+		int deleted;
+		if (top_ < size_) {
+			deleted = this->buff[top_];
+			this->buff[top_] = 0;
+			this->top_++;
+		}
+		else {
+			deleted = this->buff[top_];
+			this->buff[top_] = 0;
+			this->top_ = 0;
+		}
+		if (real_size_ > 0) {
+			this->real_size_--;
+		}
+		else {
+			this->real_size_ = 0;
+		}
+		return deleted;
+	}
+	
+	//значение верхнего элемента
+	int first(){
+		if (top_ == size_) {
+			this->top_ = 0;
+		}
+		return this->buff[top_];
+	}
+	
+	//размер буфера
+	int size_of() {
+		return this->buff.size();
+	}
+
+	//флаг пустого буфера
+	bool isEmpty() {
+		if (this->real_size_ == 0) { return true; }
+		else return false;
+	}
+
+
+};
+
+int main() {
+	int c = 0;
+	Queue Buffer(10);
+	setlocale(LC_ALL, "Russian");
+	cout << "========= Проверка буфера ==========";
+	
+	for (int i = 0; i < 10; i++) {
+		Buffer.enqueue(i+1);
+	}
+	for (int i = 0; i < 3; i++) {
+			cout << "\n Удаляю элемент [" << i+1 << "] " << Buffer.first();
+			Buffer.dequeue();
+	}
+	for (int i = 0; i < 10; i++) {
+		cout << "\nЭлемент [" << i + 1 << "] " << Buffer.first();
+		cout << "\nЭлемент [" << i + 1 << "] " << Buffer.first();
+		Buffer.dequeue();
+	}
+	if (Buffer.isEmpty()) { cout << "\nБуффер пуст!"; }
+	return 0;
+}
